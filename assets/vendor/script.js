deb62a165d68b0fe63487b5f380c471398e076c0
PRDXN95 <dhanrajs@axioned.com>
2022-04-28 14:43:08 +0530

    added checkLocalStorage function

--- assets/vendor/script.js
+++ assets/vendor/script.js
@@ -3,13 +3,12 @@ $(document).ready(function () {
   var search_btn = $('#search-btn');
   var list_items = [];
 
-  //on search button click page redirects to cat breed details page and cat breed info is displayed.
+  //on search button click first clears local storage thne page is redirected to cat breed details page and cat breed info is displayed.
   search_btn.click(function (e) { 
     e.preventDefault();
     getCatInfo();
-    location.href = 'cat_breed_details.html'
   });
- 
+
   //function to get cat breed details data from cat api and stores it in local storage and then calls getImages 
   function getCatInfo() {
     $.ajax({
@@ -18,10 +17,7 @@ $(document).ready(function () {
       headers: {'x-api-key' : 'c01bb41e-223c-4c42-8d3f-32776d4d64a9'},
       data: 'JSON',
       success: function(response) {
-        console.log(response);
-        var breed_info = JSON.stringify(response);
-        localStorage.setItem('breed_details', breed_info);
-        // console.log(localStorage)
+        localStorage.setItem('breed_details', JSON.stringify(response));
         var search_input_value = search_input.val().substring(0,4);
         getImages(search_input_value);
       }
@@ -36,19 +32,28 @@ $(document).ready(function () {
       headers: {'x-api-key' : 'c01bb41e-223c-4c42-8d3f-32776d4d64a9'},
       data: 'JSON',
       success: function(response) {
-        var image_response = JSON.stringify(response);
-        localStorage.setItem('imageResponse',image_response);
+        // var image_response = JSON.stringify(response);
+        localStorage.setItem('image_response',JSON.stringify(response));
+        checkLocalStorage();
       }
     });
   }
 
+  // redirects to cat breed details page if local storage is not empty.
+  function checkLocalStorage() {
+    if (localStorage.getItem('breed_details') !== null) {
+      location.href = 'cat_breed_details.html' 
+    } 
+  }
+  
   //function to display cat breed details and other cat photos once the page is redirected to cat breed details.
   function displayCatBreed() {
     if($('section').hasClass('cat-breed-details')) {
       console.log(localStorage);
-      var image_data = JSON.parse(localStorage.getItem('imageResponse'));
+      var image_data = JSON.parse(localStorage.getItem('image_response'));
       var breed_info_data = JSON.parse(localStorage.getItem('breed_details'));
-      console.log(breed_info_data);
+      console.log(image_data);
+      var cat_breed_name = breed_info_data[0]['name'];
       var cat_temperament = breed_info_data[0]['temperament'];
       var cat_origin = breed_info_data[0]['origin'];
       var cat_life_span = breed_info_data[0]['life_span'];
@@ -60,6 +65,7 @@ $(document).ready(function () {
       var cat_health_issues = breed_info_data[0]['health_issues'];
       var cat_social_needs = breed_info_data[0]['social_needs'];
       var cat_stranger_friendly = breed_info_data[0]['stranger_friendly'];
+      $('.breed-name-heading').html(cat_breed_name);
       $('.temperament').html(cat_temperament);
       $.each(image_data, function (index, value) { 
         var li = document.createElement('li');
@@ -72,5 +78,16 @@ $(document).ready(function () {
   }
 
   displayCatBreed();
+
+  function deleteLocalStorageValues() {
+    // console.log(image_response);
+    if (localStorage.getItem('breed_details') !== null) {
+      localStorage.removeItem('breed_details');
+    } 
+    if (localStorage.getItem('image_response') !== null) {
+      localStorage.removeItem('image_response');
+    }
+  }
+  deleteLocalStorageValues();
 
 });
