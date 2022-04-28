9b428932ada6faaa86dc58244b8e907b6dae4f0f
PRDXN95 <dhanrajs@axioned.com>
2022-04-28 12:58:32 +0530

    added displayCatBreed function

--- assets/vendor/script.js
+++ assets/vendor/script.js
@@ -18,8 +18,10 @@ $(document).ready(function () {
       headers: {'x-api-key' : 'c01bb41e-223c-4c42-8d3f-32776d4d64a9'},
       data: 'JSON',
       success: function(response) {
+        console.log(response);
         var breed_info = JSON.stringify(response);
         localStorage.setItem('breed_details', breed_info);
+        // console.log(localStorage)
         var search_input_value = search_input.val().substring(0,4);
         getImages(search_input_value);
       }
@@ -39,5 +41,36 @@ $(document).ready(function () {
       }
     });
   }
+
+  //function to display cat breed details and other cat photos once the page is redirected to cat breed details.
+  function displayCatBreed() {
+    if($('section').hasClass('cat-breed-details')) {
+      console.log(localStorage);
+      var image_data = JSON.parse(localStorage.getItem('imageResponse'));
+      var breed_info_data = JSON.parse(localStorage.getItem('breed_details'));
+      console.log(breed_info_data);
+      var cat_temperament = breed_info_data[0]['temperament'];
+      var cat_origin = breed_info_data[0]['origin'];
+      var cat_life_span = breed_info_data[0]['life_span'];
+      var cat_adaptability = breed_info_data[0]['adaptability'];
+      var cat_affection_level = breed_info_data[0]['affection_level'];
+      var cat_child_friendly = breed_info_data[0]['child_friendly'];
+      var cat_grooming = breed_info_data[0]['grooming'];
+      var cat_intelligence = breed_info_data[0]['intelligence'];
+      var cat_health_issues = breed_info_data[0]['health_issues'];
+      var cat_social_needs = breed_info_data[0]['social_needs'];
+      var cat_stranger_friendly = breed_info_data[0]['stranger_friendly'];
+      $('.temperament').html(cat_temperament);
+      $.each(image_data, function (index, value) { 
+        var li = document.createElement('li');
+        list_items.push(li);
+        li.classList.add('other-photo-item');
+        $(li).html(`<img src=${value.url}>`);
+        $('.photos').append(li);
+      });
+    }
+  }
+
+  displayCatBreed();
 
 });
